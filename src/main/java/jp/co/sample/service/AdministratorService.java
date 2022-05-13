e2bca7b73b85163f9e6452d4611ba5798fdf5dce
sakuraikarin <karin.sakurai@rakus-partners.co.jp>
2022-05-13 15:16:19 +0900

    AdministratorServiceの作成

--- src/main/java/jp/co/sample/service/AdministratorService.java
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.repository.AdministratorRepository;
 
 @Service
@@ -12,6 +13,10 @@ public class AdministratorService {
 	
 	@Autowired
 	private AdministratorRepository repository;
+	
+	public void insert(Administrator administrator) {
+		repository.insert(administrator);
+	}
 	
 
 }
